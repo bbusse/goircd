3bfc100054031863c7e18cc77ba27126486adb7f
Björn Busse <mail@baerlin.eu>
2015-10-18 22:44:03 +0200

    replace spaces with tab

--- client.go
+++ client.go
@@ -54,7 +54,7 @@ type Client struct {
 func (c Client) String() string {
 	addr := c.conn.RemoteAddr().String()
 	i := strings.LastIndex(addr, ":")
-    addr = addr[0:i]
+	addr = addr[0:i]
 	domain, err := net.LookupAddr(addr)
 	if err == nil {
 		addr = strings.TrimSuffix(domain[0], ".")
